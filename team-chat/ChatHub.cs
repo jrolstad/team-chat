2454da7f3d385e00b4ecc1f348c77576ddd0aac9
Josh Rolstad <jrolstad@gmail.com>
2015-01-29 18:07:51 -0800

    Simple implementation

--- team-chat/ChatHub.cs
+++ team-chat/ChatHub.cs
@@ -1,19 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR;
 
 namespace team_chat
 {
     public class ChatHub : Hub
     {
-        public void Send(string name, string message)
+        public void Send(string message)
         {
-            // Call the broadcastMessage method to update clients.
-            Clients.All.broadcastMessage(name, message);
+            var userName = GetUserName();
+            Clients.All.broadcastMessage(userName, message);
 
-            this.Context.User.Identity.Name
+            _dbContext.Messages.Add(message);
+            saveChanges
+        }
+
+        public override Task OnConnected()
+        {
+            var messages = _dbContext.Messages();
+
+            this.Clients.Caller.broadcastMessage("foo", "connection message");
+
+            return base.OnConnected();
+        }
+
+        private string GetUserName()
+        {
+            var userName = "N/A";
+
+            if (this.Context != null
+                && this.Context.User != null
+                && this.Context.User.Identity != null)
+            {
+                userName = this.Context.User.Identity.Name;
+            }
+            return userName;
         }
     }
 }
